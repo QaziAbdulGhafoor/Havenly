bcdfe62912fcc89bf607880d99712b2720074bc7
QaziAbdulGhafoor <qzgh986@gmail.com>
2026-05-24 17:44:34 +0500

    styled detailed listings

--- init/index.js
+++ init/index.js
@@ -17,7 +17,7 @@ let addListings = async () => {
   let del = await Listing.deleteMany({});
   sampleListings.data = sampleListings.data.map((obj) => ({
     ...obj,
-    owner: "69f497bbc44b577734b1c7e2",
+    owner: "69f58db1e54b17760d6efcc8",
   }));
   let res = await Listing.insertMany(sampleListings.data);
   console.log("all added");
